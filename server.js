eb9ca2d810bb20e73d4020d1239414104399f245
Mike Walshe <mjwalshe@gmail.com>
2020-12-22 10:16:05 +0000

    Add express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('home');
+})
+
+app.listen(PORT, () => {
+  console.log(`Listenining on ${PORT}`);
+})
+
+
+
+
