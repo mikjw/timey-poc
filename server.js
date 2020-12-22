d609b56a40307c15f90c1b4e30e80ba05f158e15
Mike Walshe <mjwalshe@gmail.com>
2020-12-22 11:26:14 +0000

    Add mongo connection

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const PORT = 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("Connected to Atlas");
+})
 
 app.get('/', (req, res) => {
   res.send('home');
