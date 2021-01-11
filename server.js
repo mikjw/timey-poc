24095db251251f55ea4f38fe350a82952b4f2d5f
mikjw <mjwalshe@gmail.com>
2021-01-11 19:11:28 +0000

    Add register and login

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const timesRouter = require('./routes/timeRoutes');
 const usersRouter = require('./routes/userRoutes');
+
+const User = require('./models/user.model');
 
 const app = express();
 const PORT = 5001;
@@ -12,11 +15,44 @@ const PORT = 5001;
 app.use(express.json());
 app.use(cors());
 
+app.post('/register', async (req, res) => {
+  const email = req.body.email;
+  const password = await bcrypt.hash(req.body.password, 12);
+  const newUser = new User({
+    email,
+    password
+  })
+  newUser.save()
+  .then(() => {
+    res.json('User added succesfully'),
+    console.log(newUser);
+  })
+  .catch(err => 
+    console.error(err),
+    res.status(400).json('Email already in use'))
+});
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email })
+  .then(user => {
+    if (!user) { 
+      res.json('Email or password invalid');
+    }
+    else {
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) console.log('Error: ' + err);
+        else if (result === true) {res.json('Success') }
+        else { res.json('Email or password invalid') }
+      })
+    }
+  })
+  .catch(err => res.status(400).json('Error: ' + err))
+});
+
 app.use('/times', timesRouter);
 app.use('/users', usersRouter);
 
 const uri = process.env.ATLAS_URI;
-// mongoose.set();
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true } );
 const connection = mongoose.connection;
 connection.once('open', () => {
