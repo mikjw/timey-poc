1d99465c9a22cf2e511caaf7836b530ce33e4a67
mikjw <mjwalshe@gmail.com>
2021-03-20 23:12:51 +0000

    Fix time get routes

--- routes/timeRoutes.js
+++ routes/timeRoutes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 const Time = require('../models/time.model')
+const { checkAuthentication } = require('../authHelper');
 
-router.route('/').get((req, res) => {
+router.route('/:id').get(checkAuthentication, (req, res) => {
+  Time.findById(req.params.id)
+  .then(time => {
+    res.json(time);
+  })
+})
+
+router.route('/user/:user').get(checkAuthentication, (req, res) => {
+  Time.find({ user: req.params.user })
+  .then(times => {
+    res.json(times);
+  })
+  .catch(err => res.status(400).json('Error: ' + err));
+})
+
+router.route('/all').get(checkAuthentication, (req, res) => {
   Time.find()
   .then(times => {
     res.json(times);
@@ -10,16 +26,14 @@ router.route('/').get((req, res) => {
 })
 
 router.route('/add').post((req, res) => {
-  console.log(req.body);
   const title = req.body.title;
   const seconds = Number(req.body.seconds);
-  const newTime = new Time({
-    title,
-    seconds
-  });
+  const workspace = req.body.workspace;
+  const user = req.body.user;
+  const newTime = new Time({ title, seconds, workspace, user });
 
   newTime.save()
-  .then(() => res.json('Time added succesfully'))
+  .then(() => res.json(newTime._id))
   .catch(err => res.status(400).json('Error: ' + err));
 })
 
@@ -33,6 +47,12 @@ router.route('/update/:id').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
-  })
+})
+
+router.route('/:id').delete((req, res) => {
+  Time.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Time deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
