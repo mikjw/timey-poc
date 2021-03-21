adc87eddd9bff81fbbc24c322a5ede07804d8d5f
mikjw <mjwalshe@gmail.com>
2021-03-21 23:05:19 +0000

    Add mock data

---
+++ test/mockResponse.js
@@ -0,0 +1,58 @@
+const first = () => {
+  const data = 
+    [
+      {
+        _id: '605681a6cfc68c87b47d97db',
+        title: 'Homework',
+        seconds: 469,
+        workspace: '605502c25db13d06230179c9',
+        user: '605680eccfc68c87b47d97da',
+        __v: 0
+      },
+      {
+        _id: '605681aacfc68c87b47d97dc',
+        title: 'Singing',
+        seconds: 108,
+        workspace: '605502c65db13d06230179ca',
+        user: '605680eccfc68c87b47d97da',
+        __v: 0
+      },
+      {
+        _id: '605681adcfc68c87b47d97dd',
+        title: 'Sewing',
+        seconds: 221,
+        workspace: '605502cb5db13d06230179cb',
+        user: '605680eccfc68c87b47d97da',
+        __v: 0
+      }
+    ]
+  return data;
+}
+
+const second = () => {
+  const data = 
+    [
+      {
+        _id: '605681a6cfc63hh7b47d97db',
+        title: 'Work',
+        seconds: 429,
+        workspace: '605502c25db13d06230179c9',
+        user: '605680eccfc68c87b47d97da',
+        __v: 0
+      },
+      {
+        _id: '605681a6cf123hh7b47d97db',
+        title: 'Schmerk',
+        seconds: 22,
+        workspace: '605502c25db13d06230179c9',
+        user: '605680eccfc68c87b47d97da',
+        __v: 0
+      }
+    ]
+  return data;
+}
+
+module.exports = {
+  first,
+  second
+}
