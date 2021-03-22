f49db3695a6db753216f12183c61849af5fe6617
mikjw <mjwalshe@gmail.com>
2021-03-22 00:01:02 +0000

    Add analytics route

---
+++ routes/analyticsRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Workspace = require('../models/workspace.model')
+const Time = require('../models/time.model')
+const analyticsService = require('../services/analytics.service');
+const { checkAuthentication } = require('../helpers/authHelper');
+
+router.route('/:user').get(checkAuthentication, (req, res) => {
+  Time.find({ user: req.params.user }).lean()
+  .then(time => {
+    res.json(analyticsService.getAnalytics(time));
+  })
+})
+
+module.exports = router; 
