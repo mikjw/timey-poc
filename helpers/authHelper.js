70258c92f2193d7252e08a475bbca1a99bda3f1d
mikjw <mjwalshe@gmail.com>
2021-03-22 00:46:26 +0000

    Update auth checker

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,10 +1,8 @@
 module.exports = {
   checkAuthentication: function (req, res, next) {
     if (req.isAuthenticated()) { 
-      console.log('AUTH YES')
       return next() 
     } 
-    console.log('AUTH NO')
     res.sendStatus(403);
     }
   }
