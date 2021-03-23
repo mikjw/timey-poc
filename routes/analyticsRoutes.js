5fdc4bc70fdc3e79d29b1c10cc3d658dce515ccd
mikjw <mjwalshe@gmail.com>
2021-03-23 12:51:06 +0000

    Tidy analytics route

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -3,10 +3,15 @@ const Time = require('../models/time.model')
 const analyticsService = require('../services/analytics.service');
 const { checkAuthentication } = require('../helpers/authHelper');
 
+
+/**
+ * Get analytics for passed-in user
+ */
+
 router.route('/:user').get(checkAuthentication, (req, res) => {
   Time.find({ user: req.params.user }).lean()
-  .then(time => {
-    res.json(analyticsService.getAnalytics(time));
+  .then(times => {
+    res.json(analyticsService.getAnalytics(times));
   })
 })
 
